9d69c1d69e2d618d0b051f7de6a3853c90e90e38
chenjiegd <chenjiegd@foxmail.com>
2019-06-18 15:27:20 +0800

    Update main.py

--- PCA9685-Python-Microbit-master/main.py
+++ PCA9685-Python-Microbit-master/main.py
@@ -4,14 +4,14 @@ import servo
 import motor
 
 # Initialise the PCA9685 using the default address (0x40).
-pwm = PCA9685.PCA9685(i2c)
+# pwm = PCA9685.PCA9685(i2c)
 
 # Configure min and max servo pulse lengths (will need to adjust for different servos)
-servo_min = 150  # Min pulse length out of 4096
-servo_max = 600  # Max pulse length out of 4096
+# servo_min = 150  # Min pulse length out of 4096
+# servo_max = 600  # Max pulse length out of 4096
 
 # Set frequency to 60hz, good for servos.
-pwm.set_pwm_freq(60)
+# pwm.set_pwm_freq(60)
 
 # print('Moving servo on channel 0, press Ctrl-C to quit...')
 
