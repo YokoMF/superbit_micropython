db96d70f790a9ab7a9d137ce80b019072859b799
chenjiegd <chenjiegd@foxmail.com>
2019-06-18 15:26:05 +0800

    新增motor.py
    MotorRun测试正常

--- PCA9685-Python-Microbit-master/main.py
+++ PCA9685-Python-Microbit-master/main.py
@@ -1,6 +1,7 @@
 from microbit import sleep, i2c
 import PCA9685
 import servo
+import motor
 
 # Initialise the PCA9685 using the default address (0x40).
 pwm = PCA9685.PCA9685(i2c)
@@ -20,8 +21,9 @@ pwm.set_pwm_freq(60)
 # pwm.set_pwm(3, 0, servo_max)
 # sleep(1000)
 
-# # Use servo helper class to move channel 0 by degrees
-# s0 = servo.Servos(i2c)
+# 驱动舵机
+# Use servo helper class to move channel 0 by degrees
+s0 = servo.Servos(i2c)
 # s0.position(4, 0)
 # s0.position(5, 0)
 # sleep(1000)
@@ -30,13 +32,13 @@ pwm.set_pwm_freq(60)
 # sleep(1000)
 # s0.release(0)
 
-while True:
-	# # S2舵机0->90->180->90，循环转动
-	# s0.position(4, 0)
-	# sleep(1000)
-	# s0.position(4, 90)
-	# sleep(1000)
-	# s0.position(4, 180)
-	# sleep(1000)
-	# s0.position(90)
-	# sleep(1000)
+
+# 驱动电机
+m1 = motor.DCMotors(i2c)
+m1.MotorRun(1, 255)
+sleep(1000)
+
+# pwm.set_pwm(11, 0, 4095)
+# pwm.set_pwm(10, 0, 0)
+# pwm.set_pwm(8, 0, 4095)
+# pwm.set_pwm(9, 0, 0)
